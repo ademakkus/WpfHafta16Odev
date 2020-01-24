1774a5f2db21c334a55dbdf6afe15af163419306
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-01-24 12:13:25 +0300

    Product class completed

---
+++ AdemAKKUS.DbHelpers/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Configuration;
+
+
+namespace AdemAKKUS.DbHelpers
+{
+    public class Helper
+    {
+    protected static readonly string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+  }
+}
