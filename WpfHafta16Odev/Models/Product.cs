fb57f236a14d83c360d287d8029e3d3ceb049a67
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-01-24 12:17:58 +0300

    Product folder directory is changed

--- WpfHafta16Odev/Models/Product.cs
+++ WpfHafta16Odev/Models/Product.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ *  [ProductID], 
+                  [ProductName], 
+                  [SupplierID], 
+                  [CategoryID], 
+                  [QuantityPerUnit], 
+                  [UnitPrice], 
+                  [UnitsInStock], 
+                  [UnitsOnOrder], 
+                  [ReorderLevel], 
+                  [Discontinued]
+ * */
 namespace Vektorel.DbHelpers.Models
 {
   class Product
   {
+    public int ProductID { get; set; }
+    public string ProductName { get; set; }
+    public int SupplierID { get; set; }
+    public int CategoryID { get; set; }
+    public string QuantityPerUnit { get; set; }
+    public int UnitPrice { get; set; }
+    public int UnitsInStock { get; set; }
+    public int UnitsOnOrder { get; set; }
+    public int ReorderLevel { get; set; }
+    public bool Discontinued { get; set; }
+    public Product(int productID,string productName,int supplierID,int categoryID,string quantityPerUnit,int unitPrice,int unitsInStock,int unitsOnOrder,int reorderLevel,bool discontinued)
+  
+    {
+      ProductID = productID;
+      ProductName = productName;
+      SupplierID = supplierID;
+      CategoryID = categoryID;
+      QuantityPerUnit = quantityPerUnit;
+      UnitPrice = unitPrice;
+      UnitsInStock = unitsInStock;
+      UnitsOnOrder = unitsOnOrder;
+      ReorderLevel = reorderLevel;
+      Discontinued = discontinued;
+    }
+    public Product()
+    {
+
+    }
   }
 }
